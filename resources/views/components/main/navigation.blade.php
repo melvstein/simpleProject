f0e3b34ad133be2e65372e15995b3e99d5c5bf3c
Melvs <melvinbayogo@gmail.com>
2021-04-22 00:29:14 +0800

    Quick Update

--- resources/views/components/main/navigation.blade.php
+++ resources/views/components/main/navigation.blade.php
@@ -26,7 +26,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
                 </button>
-                <div x-show="isOpen()" @click.away="close" class="absolute shadow border top-16 right-0 bg-blue-300 rounded-l-lg">
+                <div x-show="isOpen()" @click.away="close" class="absolute shadow border top-16 right-0 bg-blue-300 rounded-l-lg z-10">
                     <div class="flex flex-col">
                         <a href="{{ route('login') }}" class="text-white hover:bg-blue-500 hover:bg-opacity-50 rounded-tl-lg p-4 {{ request()->routeIs('login') ? 'bg-blue-900 bg-opacity-50' : '' }}">Login</a>
                         <a href="{{ route('register') }}" class="text-white hover:bg-blue-500 hover:bg-opacity-50 rounded-bl-lg p-4 {{ request()->routeIs('register') ? 'bg-blue-900 bg-opacity-50' : '' }}">Register</a>
